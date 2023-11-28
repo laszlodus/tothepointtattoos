babed69501e474b67b3c8fe670fa5ba82de72245
laszlodus <79lgg4@gmail.com>
2023-11-28 17:27:52 +0000

    booking page done

---
+++ script.js
@@ -0,0 +1,17 @@
+const button = document.getElementById("showe");
+const form = document.getElementById("form");
+const picture = document.getElementById("picture");
+
+button.addEventListener("click", () => {
+    form.classList.toggle('hidden'); 
+    picture.classList.toggle('picture2');
+    button.classList.toggle('showe1');
+    document.getElementById('firstname').value = '';
+    document.getElementById('lastname').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('phonenumber').value = '';
+    document.getElementById('age').value = '';
+    document.getElementById('tattoidea').value = '';
+    document.getElementById('bodypart').value = '';
+    document.getElementById('gdpr').checked = false;
+});
