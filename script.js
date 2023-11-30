4ef2f782ae1c44b7260d07a9f314916851273a13
laszlodus <79lgg4@gmail.com>
2023-11-30 14:30:43 +0000

    new booking

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-const button = document.getElementById("showe");
-const form = document.getElementById("form");
-const picture = document.getElementById("picture");
 
-button.addEventListener("click", () => {
-    form.classList.toggle('hidden'); 
-    picture.classList.toggle('picture2');
-    button.classList.toggle('showe1');
-    document.getElementById('firstname').value = '';
-    document.getElementById('lastname').value = '';
-    document.getElementById('email').value = '';
-    document.getElementById('phonenumber').value = '';
-    document.getElementById('age').value = '';
-    document.getElementById('tattoidea').value = '';
-    document.getElementById('bodypart').value = '';
-    document.getElementById('gdpr').checked = false;
-});
 
 function submitForm() {
     // Here you can add your form submission logic, for example using AJAX
