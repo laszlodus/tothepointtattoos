c1d23114221859ec78683a9f01266578511bd140
laszlodus <79lgg4@gmail.com>
2023-11-30 12:14:30 +0000

    new javascript

--- script.js
+++ script.js
@@ -16,10 +16,12 @@ button.addEventListener("click", () => {
     document.getElementById('gdpr').checked = false;
 });
 
-<script>
-  window.onload = function () {
-    if (window.history.replaceState) {
-      window.history.replaceState(null, null, window.location.href);
-    }
-  };
-</script>
+function submitForm() {
+    // Here you can add your form submission logic, for example using AJAX
+    
+    // Simulating a form submission delay (1 second)
+    setTimeout(function() {
+      // Clear the form after 1 second
+      document.getElementById("myForm").reset();
+    }, 1000);
+  }
