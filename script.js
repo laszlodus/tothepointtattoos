e7ab32e50b987b0ade95f79476152d6707ebf934
laszlodus <79lgg4@gmail.com>
2023-12-22 13:40:49 +0000

    new code added a form

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('myForm');
+    const sendButton = document.querySelector('button[type="submit"]');
+    const firstNameInput = document.getElementById('firstname');
+    const lastNameInput = document.getElementById('lastname');
+    const emailInput = document.getElementById('email');
+    const phoneInput = document.getElementById('phonenumber');
+    const agreeCheckbox = document.getElementById('gdpr');
+
+    function updateButtonState() {
+        sendButton.disabled = !(
+            firstNameInput.value.trim() !== '' &&
+            lastNameInput.value.trim() !== '' &&
+            emailInput.checkValidity() &&
+            phoneInput.value.trim() !== '' &&
+            agreeCheckbox.checked
+        );
+
+        if (sendButton.disabled) {
+            sendButton.style.backgroundColor = 'gray'; // Szürke háttérszín
+            sendButton.innerHTML = '<i class="fas fa-times"></i> Disabled'; // Disabled ikon
+        } else {
+            sendButton.style.backgroundColor = ''; // Alaphelyzetű háttérszín
+            sendButton.innerHTML = 'Send'; // Alaphelyzetű szöveg
+        }
+    }
+
+    updateButtonState();
+
+    form.addEventListener('input', updateButtonState);
+});
 
 
 function submitForm() {
-    // Here you can add your form submission logic, for example using AJAX
-    
-    // Simulating a form submission delay (1 second)
-    setTimeout(function() {
-      // Clear the form after 1 second
-      document.getElementById("myForm").reset();
-    }, 1000);
-  }
+  setTimeout(function() {
+   document.getElementById("myForm").reset();
+  }, 1000);
+}
+
+
